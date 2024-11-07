19902730a25c7675bb3463a7ed6ed05f71208eaa
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-07 08:01:28 -0300

    feat: handle toggleDone action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { BiEdit, BiTrash } from "react-icons/bi";
 export default function Home() {
   const [list, dispatch] = useReducer(listReducer, []);
   const [newItem, setNewItem] = useState("");
+  const [isDone, setIsDone] = useState(false);
 
   const handleAdd = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -22,30 +23,48 @@ export default function Home() {
     setNewItem("");
   };
 
+  const handleDone = (itemId: number) => {
+    dispatch({
+      type: "toggleDone",
+      payload: { id: itemId },
+    });
+
+    setIsDone(!isDone);
+  };
+
   return (
     <main className="max-w-96 p-4 mx-auto">
-      <form onSubmit={handleAdd} className="h-10 my-8 flex items-center justify-between gap-1">
+      <form
+        onSubmit={handleAdd}
+        className="h-10 my-8 flex items-center justify-between gap-1"
+      >
         <input
           className="border w-full h-full"
           type="text"
           value={newItem}
           onChange={(e) => setNewItem(e.target.value)}
         />
-        <button className="h-full px-2 bg-slate-300 hover:opacity-65">adicionar</button>
+        <button className="h-full px-2 bg-slate-300 hover:opacity-65">
+          adicionar
+        </button>
       </form>
 
       {list.map((item) => {
         return (
-          <div
+          <button
             key={item.id}
-            className="my-2 p-2 border flex items-center justify-between bg-slate-100"
+            type="button"
+            onClick={() => handleDone(item.id)}
+            className={`w-full my-2 p-2 border flex items-center justify-between bg-slate-100 ${
+              isDone && "opacity-40 [&>*]:pointer-events-none"
+            }`}
           >
-            <p>{item.text}</p>
+            <p className={`${isDone && "line-through"}`}>{item.text}</p>
             <div className="flex items-center gap-2 [&>*]:cursor-pointer">
               <BiTrash size={24} className="text-red-700 hover:opacity-65" />
               <BiEdit size={24} className="text-green-700 hover:opacity-65" />
             </div>
-          </div>
+          </button>
         );
       })}
     </main>
