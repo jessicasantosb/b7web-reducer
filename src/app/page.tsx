29be4b9f96eb2c2aaa551b000269fe3f98f70947
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-07 07:39:54 -0300

    feat: add new item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,45 @@
 "use client";
 
+import { FormEvent, useReducer, useState } from "react";
+
 import { listReducer } from "@/reducers/listReducer";
-import { useReducer } from "react";
 
 export default function Home() {
   const [list, dispatch] = useReducer(listReducer, []);
+  const [newItem, setNewItem] = useState("");
 
-  const handleAdd = () => {
+  const handleAdd = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
     dispatch({
       type: "add",
       payload: {
-        text: "Novo item",
+        text: newItem,
       },
     });
 
-    console.log(list);
+    setNewItem("");
   };
 
   return (
-    <main>
-      <button onClick={handleAdd}>adicionar</button>
+    <main className="p-4">
+      <form onSubmit={handleAdd}>
+        <input
+          className="border"
+          type="text"
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+        />
+        <button>adicionar</button>
+      </form>
+
+      {list.map((item) => {
+        return (
+          <div key={item.id} className="flex justify-between">
+            <p>{item.text}</p>
+          </div>
+        );
+      })}
     </main>
   );
 }
