7a662a06585076af985b2c1c593284ca054f0a7f
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-07 07:50:07 -0300

    style: create layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { FormEvent, useReducer, useState } from "react";
 
 import { listReducer } from "@/reducers/listReducer";
+import { BiEdit, BiTrash } from "react-icons/bi";
 
 export default function Home() {
   const [list, dispatch] = useReducer(listReducer, []);
@@ -22,21 +23,28 @@ export default function Home() {
   };
 
   return (
-    <main className="p-4">
-      <form onSubmit={handleAdd}>
+    <main className="max-w-96 p-4 mx-auto">
+      <form onSubmit={handleAdd} className="h-10 my-8 flex items-center justify-between gap-1">
         <input
-          className="border"
+          className="border w-full h-full"
           type="text"
           value={newItem}
           onChange={(e) => setNewItem(e.target.value)}
         />
-        <button>adicionar</button>
+        <button className="h-full px-2 bg-slate-300 hover:opacity-65">adicionar</button>
       </form>
 
       {list.map((item) => {
         return (
-          <div key={item.id} className="flex justify-between">
+          <div
+            key={item.id}
+            className="my-2 p-2 border flex items-center justify-between bg-slate-100"
+          >
             <p>{item.text}</p>
+            <div className="flex items-center gap-2 [&>*]:cursor-pointer">
+              <BiTrash size={24} className="text-red-700 hover:opacity-65" />
+              <BiEdit size={24} className="text-green-700 hover:opacity-65" />
+            </div>
           </div>
         );
       })}
